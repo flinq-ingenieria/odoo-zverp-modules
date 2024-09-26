00da14a6f3ab797a94ec0a15c0d19b07585ed65b
Pablo Castelo <pablocv@gmx.es>
2024-09-26 15:56:48 +0200

    check

--- account_invoice_related_document/models/account_move.py
+++ account_invoice_related_document/models/account_move.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 class AccountMove(models.Model):
     _inherit = 'account.move'
@@ -14,21 +14,21 @@ class AccountMove(models.Model):
         store=False
     )
 
+    @api.depends('invoice_origin')
     def _compute_related_document(self):
         for record in self:
             related_document = False
+
             sale_order = self.env['sale.order'].search([('name', '=', record.invoice_origin)], limit=1)
             if sale_order:
-                related_document = f'sale.order,{sale_order.id}'
-                continue
+                record.related_document = f'sale.order,{sale_order.id}'
+
             elif self.env['ir.model']._get('purchase.order'):
                 purchase_order = self.env['purchase.order'].search([('name', '=', record.invoice_origin)], limit=1)
                 if purchase_order:
-                    related_document = f'purchase.order,{purchase_order.id}'
-                    continue
+                    record.related_document = f'purchase.order,{purchase_order.id}'
+
             elif self.env['ir.model']._get('contract.contract'):
                 contract = self.env['contract.contract'].name_search(name=record.invoice_origin, operator='=', limit=1)
                 if contract:
-                    related_document = f'contract.contract,{contract[0][0]}'
-
-        record.related_document = related_document
+                    record.related_document = f'contract.contract,{contract[0][0]}'
