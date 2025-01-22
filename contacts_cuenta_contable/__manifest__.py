702399c862b269869e9ab0dcd1ebf93455bf1d54
Pablo Castelo <pablocv@gmx.es>
2025-01-22 08:40:33 +0100

    listar cuentas contables

---
+++ contacts_cuenta_contable/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Partner Tree Account Fields",
+    "version": "16.0.1.0.0",
+    "summary": "Extends partner tree view to include account fields",
+    "author": "zvERP.com",
+    "website": "https://www.zverp.com",
+    "category": "Customizations",
+    "depends": ["contacts", "account"],
+    "data": [
+        "views/res_partner_view.xml",
+    ],
+    "installable": True,
+    "application": False,
+    "license": "AGPL-3"
+}
