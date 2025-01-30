ccf8a03df0ecd6397260de2b50a863097c9e02b7
Pablo Castelo <pablocv@gmx.es>
2025-01-30 13:24:32 +0100

    add invoice_calendar

---
+++ invoice_calendar/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Account Invoice Calendar',
+    'version': '16.0.1.0.0',
+    'category': 'Accounting',
+    'summary': 'Shows unpaid invoice due dates in the calendar.',
+    'description': 'Display due dates of unpaid invoices in the calendar view.',
+    'author': 'zvERP.com',
+    'license': 'AGPL-3',
+    'depends': ['account'],
+    'data': [
+        'views/account_invoice_calendar_view.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
---
+++ invoice_calendar/models/models.py
@@ -0,0 +1,25 @@
+from odoo import models, fields
+
+class AccountMove(models.Model):
+    _inherit = "account.move"
+
+    color_index = fields.Integer(
+        string="Color Index",
+        compute="_compute_color_index",
+        store=True
+    )
+
+    def _compute_color_index(self):
+        for record in self:
+            if record.move_type == 'in_invoice':
+                if record.payment_state == 'paid':
+                    record.color_index = 10
+                else:
+                    record.color_index = 1
+            elif record.move_type == 'out_invoice':
+                if record.payment_state == 'paid':
+                    record.color_index = 10
+                else:
+                    record.color_index = 2
+            else:
+                record.color_index = 0
