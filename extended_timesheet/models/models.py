b17f169bf8c778eb9b25eb4d184a937197dce92e
Pablo Castelo <pablocv@gmx.es>
2025-09-22 13:34:23 +0200

    fowardport billable field

--- extended_timesheet/models/models.py
+++ extended_timesheet/models/models.py
@@ -7,6 +7,7 @@ class AccountAnalyticLine(models.Model):
 	_inherit = 'account.analytic.line'
 
 	coche_personal = fields.Boolean(string="Personal car?")
+	facturable = fields.Boolean(string="Billable?", default=True)
 	kilometros = fields.Integer(string="Kilometres")
 	lugar = fields.Char(string="Mission place")
 	servicio_campo = fields.Integer(string="Lunches")
